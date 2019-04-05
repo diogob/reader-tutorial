e5e9f541627f31f243a808536b986fdd1bdf8953
Diogo Biazus <diogo@biazus.me>
2019-04-05 18:04:06 -0400

    Add CoinberryApi module to fetch coinberry prices

---
+++ src/CoinberryApi.hs
@@ -0,0 +1,30 @@
+module CoinberryApi ( price
+                    , Currency (..)
+                    ) where
+
+import Control.Monad.IO.Class
+import Data.Aeson
+import Network.HTTP.Req
+import qualified Data.Text as T
+
+price :: Currency -> IO Price
+-- price baseCurrency = undefined
+price baseCurrency = do 
+     j <- runReq defaultHttpConfig $
+         req GET (https "api.coinberry.com" /: "prices" /: "CAD" /: (T.pack $ show baseCurrency)) NoReqBody jsonResponse mempty
+     pure $ responseBody j
+
+data Price = 
+    Price { sell :: String
+          , buy :: String 
+          } deriving (Show)
+
+instance FromJSON Price where
+    parseJSON = 
+        withObject "Price" $ \obj ->
+            Price <$> ((obj .: "data") >>= (.: "sell"))
+                  <*> ((obj .: "data") >>= (.: "buy"))
+
+
+data Currency = CAD | BTC | ETH | LTC | XRP 
+    deriving (Show)
--- src/Transformer.hs
+++ src/Transformer.hs
@@ -11,11 +11,11 @@ import Control.Monad.Reader
 type App = ReaderT Context IO
 
 printURI :: App ()
-printURI = do 
+printURI = do
     ctx <- ask
-    liftIO $ (ctxPrint ctx) $ "The URI given to App is " <> uri (ctxConfig ctx)
+    liftIO $ ctxPrint ctx $ "The URI given to App is " <> uri (ctxConfig ctx)
 
 printQuiet :: App ()
 printQuiet = do
     ctx <- ask
-    liftIO $ print $ "The QUIET switch given to App is " <> show (quiet (ctxConfig ctx))
+    liftIO $ print $ "The QUIET switch given to App is " <> show (quiet $ ctxConfig ctx)
